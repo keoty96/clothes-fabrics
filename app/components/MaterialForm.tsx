3ed26568d5dd696e4d157d28ae05de5762398939
keoty96 <kosorio457@gmail.com>
2025-09-03 22:10:38 -0500

    format code

--- app/components/MaterialForm.tsx
+++ app/components/MaterialForm.tsx
@@ -1,50 +1,61 @@
-"use client"
+"use client";
 
 interface Fabrics {
-    material: string;
-    percentage: string;
+  material: string;
+  percentage: string;
 }
 
 import { useState } from "react";
 
 export default function MaterialForm() {
+  const [material, setMaterial] = useState("");
+  const [percentage, setPercentage] = useState("");
+  const [fabricList, setFabricList] = useState<Fabrics[]>([]);
 
-    const [material, setMaterial] = useState('');
-    const [percentage, setPercentage] = useState('');
-    const [fabricList, setFabricList] = useState<Fabrics[]>([])
+  const handleFabrics = () => {
+    if (material.length > 0 && percentage.length > 0) {
+      const newFabrics: Fabrics = {
+        material: material,
+        percentage: percentage,
+      };
 
-    const handleFabrics = () => {
-        if (material.length > 0 && percentage.length > 0) {
-            const newFabrics:Fabrics = {
-                material: material,
-                percentage: percentage
-            }
+      setFabricList((prev) => [...prev, newFabrics]);
 
-            setFabricList(prev => [...prev, newFabrics])
-
-            // clean
-            setMaterial('');
-            setPercentage('');
-        }
+      // clean
+      setMaterial("");
+      setPercentage("");
     }
+  };
 
-    return(
-        <div className="flex gap-4">
-            <p>Add a Material</p>
+  return (
+    <div className="flex gap-4">
+      <p>Add a Material</p>
 
-            <label>Material</label>
-            <input onChange={(e) => setMaterial(e.target.value)} value={material} type="text" name="material" placeholder="cotton" />
+      <label>Material</label>
+      <input
+        onChange={(e) => setMaterial(e.target.value)}
+        value={material}
+        type="text"
+        name="material"
+        placeholder="cotton"
+      />
 
-            <label>Percentage</label>
-            <input onChange={(e) => setPercentage(e.target.value)} value={percentage} type="text" name="percentage" placeholder="86" />
-  
-            <button onClick={handleFabrics}>Add +</button>
+      <label>Percentage</label>
+      <input
+        onChange={(e) => setPercentage(e.target.value)}
+        value={percentage}
+        type="text"
+        name="percentage"
+        placeholder="86"
+      />
 
-            { fabricList.map((fabric, index) => (
-                <div key={index}>
-                    {fabric.material} {fabric.percentage}
-                </div>
-            )) }
+      <button onClick={handleFabrics}>Add +</button>
+
+      {fabricList.map((fabric, index) => (
+        <div key={index}>
+          {fabric.material} {fabric.percentage}
         </div>
-    )
+      ))}
+    </div>
+  );
 }
