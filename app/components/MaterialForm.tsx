d0ffba2792b1c397a7e3e0ea3a2302c19460d827
keoty96 <kosorio457@gmail.com>
2025-09-08 22:43:21 -0500

    add fabric logic

--- app/components/MaterialForm.tsx
+++ app/components/MaterialForm.tsx
@@ -5,12 +5,19 @@ interface Fabrics {
   percentage: string;
 }
 
-import { useState } from "react";
+import { use, useState } from "react";
 
 export default function MaterialForm() {
+
+  const goodFabrics = ["cotton", "linen", "wool"];
+  const badFabrics = ["polyester", "viscose", "rayon", "elastene"];
+
   const [material, setMaterial] = useState("");
   const [percentage, setPercentage] = useState("");
   const [fabricList, setFabricList] = useState<Fabrics[]>([]);
+  const [counter, setCounter] = useState(0);
+  const [goodScore, setGoodScore] = useState(0);
+  const [badScore, setBadScore] = useState(0);
 
   const handleFabrics = () => {
     if (material.length > 0 && percentage.length > 0) {
@@ -21,6 +28,15 @@ export default function MaterialForm() {
 
       setFabricList((prev) => [...prev, newFabrics]);
 
+      setCounter((prev) => prev + parseInt(percentage));
+
+      if(goodFabrics.includes(material)) {
+        setGoodScore(prev => prev + parseInt(percentage))
+      }
+      if(badFabrics.includes(material)) {
+        setBadScore(prev => prev + parseInt(percentage))
+      }
+
       // clean
       setMaterial("");
       setPercentage("");
@@ -28,34 +44,61 @@ export default function MaterialForm() {
   };
 
   return (
-    <div className="flex gap-4">
-      <p>Add a Material</p>
+    <div className="grid gap-4">
+      <div className="flex gap-4 justify-center">
+        <p>Add a Material</p>
+        <label>Material</label>
+        <input
+          onChange={(e) => setMaterial(e.target.value)}
+          value={material}
+          type="text"
+          name="material"
+          placeholder="cotton"
+          className="border-2"
+        />
 
-      <label>Material</label>
-      <input
-        onChange={(e) => setMaterial(e.target.value)}
-        value={material}
-        type="text"
-        name="material"
-        placeholder="cotton"
-      />
+        <label>Percentage</label>
+        <input
+          onChange={(e) => setPercentage(e.target.value)}
+          value={percentage}
+          type="text"
+          name="percentage"
+          placeholder="86"
+          className="border-2"
+        />
+        <button className="bg-amber-300" onClick={handleFabrics}>
+          Add +
+        </button>
+      </div>
 
-      <label>Percentage</label>
-      <input
-        onChange={(e) => setPercentage(e.target.value)}
-        value={percentage}
-        type="text"
-        name="percentage"
-        placeholder="86"
-      />
+      <div className="flex w-3xl m-auto p-4 gap-2 justify-center">
+        {fabricList.map((fabric, index) => (
+          <div key={index}>
+            {fabric.material} {fabric.percentage}
+          </div>
+        ))}
+      </div>
 
-      <button onClick={handleFabrics}>Add +</button>
+      <div className="grid justify-center">
+        <p>All: {counter}</p>
+        <p>Good Score: {goodScore}</p>
+        <p>Bad Score: {badScore}</p>
+      </div>
 
-      {fabricList.map((fabric, index) => (
-        <div key={index}>
-          {fabric.material} {fabric.percentage}
-        </div>
-      ))}
+      <div className="grid justify-center">
+        { goodScore >= 70 ? 
+        <p>High Quality</p> 
+        : "" }
+
+        { goodScore >= 50 && goodScore < 70 ? 
+        <p>Medium Quality</p> 
+        : "" }
+
+        { goodScore < 50 && goodScore > 0 ? 
+        <p>Low Quality</p> 
+        : "" }
+
+      </div>
     </div>
   );
 }
