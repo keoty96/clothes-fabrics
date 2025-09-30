e1f5654ef3a3bb4e0c9daf8d982be246ad26c213
keoty96 <kosorio457@gmail.com>
2025-09-29 19:53:38 -0500

    change title

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import MaterialForm from "./components/MaterialForm";
 export default function Home() {
   return (
     <main className="bg-lime-200 text-pink-900 font-serif min-h-svh pt-16 flex flex-col gap-12">
-      <h1 className="text-6xl text-center italic">Clothes Fabrics</h1>
+      <h1 className="text-6xl text-center italic">Is It Worth Buying?</h1>
       <MaterialForm />
     </main>
   );
